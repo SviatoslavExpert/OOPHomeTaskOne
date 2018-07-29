b83a01873e564ba78c39518d08e34933c05c7eb7
SviatoslavExpert <s.granovskiy@gmail.com>
2018-07-29 10:49:39 +0300

    OOP Home Task 01

---
+++ Cat.java
@@ -0,0 +1,116 @@
+package com.gmail.s.granovskiy;
+
+/*  declaration and description of Cat class  */
+public class Cat {
+    private String name;
+    private String color;
+    private double weight;
+    private int age;
+    private int strength;
+    private int health;
+    private int intellect;
+
+    /* object constructor of Cat class */
+    Cat(String name, String color, double weight, int age, int strength, int health, int intellect) {
+        this.name = name;
+        this.color = color;
+        this.weight = weight;
+        this.age = age;
+        this.strength = strength;
+        this.health = health;
+        this.intellect = intellect;
+    }
+
+    /* getters and setters of Cat class */
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getStrength() {
+        return strength;
+    }
+
+    public void setStrength(int strength) {
+        this.strength = strength;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public void setHealth(int health) {
+        this.health = health;
+    }
+
+    public int getIntellect() {
+        return intellect;
+    }
+
+    public void setIntellect(int intellect) {
+        this.intellect = intellect;
+    }
+
+    /* methods of Cat class */
+    /* method mew */
+    public void mew() {
+        System.out.println("Mew!!!");
+    }
+
+    /* method scream */
+    public void scream() {
+        System.out.println("Mew-Mew-Mew-Mew-Mew!!!!!");
+    }
+
+    /*  method print  */
+    public void print(){
+        System.out.println("Name: " + name);
+        System.out.println("Color: " + color);
+        System.out.println("Weight: " + weight);
+        System.out.println("Age: " + age);
+        System.out.println("Strength: " + strength);
+        System.out.println("Health: " + health);
+        System.out.println("Intellect: " + intellect);
+    }
+
+    /* changeTwoProperties method  */
+    Cat changeTwoProperties(Cat a, double newWeight, int newAge){
+        a.weight = newWeight;
+        a.age = newAge;
+        return a;
+    }
+    /*  changeThreeProperties  */
+    Cat changeThreeProperties(Cat b, int newStrength, int newHealth, int newIntellect){
+        b.strength = newStrength;
+        b.health = newHealth;
+        b.intellect = newIntellect;
+        return b;
+    }
+}
---
+++ Main.java
@@ -0,0 +1,40 @@
+package com.gmail.s.granovskiy;
+
+public class Main {
+    public static void main(String[] args) {
+
+        /* catMurchik object */
+        /* properties */
+        System.out.println("These are the properties of catMurchik object: ");
+        Cat catMurchik = new Cat("Murchik", "White", 5.2, 7, 5, 7, 9);
+        catMurchik.print();
+        System.out.println();
+        /* call of method mew */
+        System.out.println("This is the work of method mew of catMurchik object:");
+        catMurchik.mew();
+        System.out.println();
+        /* call of method changeTwoProperties */
+        System.out.println("These are the changed properties: weight and age of catMurchik object: ");
+        catMurchik = catMurchik.changeTwoProperties(catMurchik, 6.5, 8);
+        catMurchik.print();
+        System.out.println();
+
+        /* catBella object */
+        /* properties */
+        System.out.println("These are the properties of catBella object: ");
+        Cat catBella = new Cat("Bella", "Grey", 3.1, 3, 4, 8, 9);
+        catBella.print();
+        System.out.println();
+        /* call of method screem */
+        System.out.println("This is the work of method screem of catBella object:");
+        catBella.scream();
+        System.out.println();
+        /* call of method changeThreeProperties */
+        System.out.println("These are the changed properties: strength, health and intellect of catBella object: ");
+        catBella.changeThreeProperties(catBella, 5, 9, 10);
+        catBella.print();
+        System.out.println();
+
+    }
+
+}
